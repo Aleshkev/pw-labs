2a45cefb318a53e4a2c01667edbadcfe528825a4
Aleshkev <43296251+Aleshkev@users.noreply.github.com>
2022-12-21 00:10:10 +0100

    labs 10: rozwiązanie

--- src/lab10shmem/gifts.c
+++ src/lab10shmem/gifts.c
@@ -30,115 +30,131 @@
 
 // Storage compartment.
 struct SharedStorage {
-    int buff[BUFFSIZE];
-    int begin;
-    int end;
-    sem_t mutex;
-    sem_t sem_free_slots;
-    sem_t sem_filled_slots;
-    int n_gifts_to_deliver;
+  int buff[BUFFSIZE];
+  int begin;
+  int end;
+  sem_t mutex;
+  sem_t sem_free_slots;
+  sem_t sem_filled_slots;
+  int n_gifts_to_deliver;
 };
 
 // popular names
-const char* elfs_names[MAX_ELFS] = { "Mirek", "Zuzia", "Gienia", "Macius", "Ela", "Stasia" };
-const char* reindeers_names[MAX_REINDEERS] = { "Janek", "Zosia", "Franek", "Jozek", "Asia", "Olek", "Ruda" };
-const char* gifts[MAX_GIFTS] = { "dolls", "legos", "a train", "a whip", "a bike" };
+const char *elfs_names[MAX_ELFS] = {"Mirek",  "Zuzia", "Gienia",
+                                    "Macius", "Ela",   "Stasia"};
+const char *reindeers_names[MAX_REINDEERS] = {
+    "Janek", "Zosia", "Franek", "Jozek", "Asia", "Olek", "Ruda"};
+const char *gifts[MAX_GIFTS] = {"dolls", "legos", "a train", "a whip",
+                                "a bike"};
 
 // Some toy-making procedure.
-int produce()
-{
-    usleep((rand() % 3) * NAP_MICROSECS);
-    return rand() % N_GIFTS;
+int produce() {
+  usleep((rand() % 3) * NAP_MICROSECS);
+  return rand() % N_GIFTS;
 }
 
 // What to do on delivery.
-void deliver(int i)
-{
-    usleep((rand() % 4) * NAP_MICROSECS);
-}
+void deliver(int i) { usleep((rand() % 4) * NAP_MICROSECS); }
 
 /**************************************************************************************************/
 // life of an elf
-int elf_main(int id, struct SharedStorage* s)
-{
-    printf("Elf %s: starting!\n", elfs_names[id]);
-    for (int i = 0; i < LOOPS; ++i) {
-        int g = produce();
-        printf("Elf %s: I produced %s\n", elfs_names[id], gifts[g]);
+int elf_main(int id, struct SharedStorage *s) {
+  printf("Elf %s: starting!\n", elfs_names[id]);
+  for (int i = 0; i < LOOPS; ++i) {
+    int g = produce();
+    printf("Elf %s: I produced %s\n", elfs_names[id], gifts[g]);
 
-        // TODO Put g into queue.
+    sem_wait(&s->sem_free_slots);
+    sem_wait(&s->mutex);
 
-        printf("Elf %s: I enqueued %s\n", elfs_names[id], gifts[g]);
-    }
-    return 0;
+    s->buff[s->end] = g;
+    s->end = (s->end + 1) % BUFFSIZE;
+
+    sem_post(&s->mutex);
+    sem_post(&s->sem_filled_slots);
+
+    printf("Elf %s: I enqueued %s\n", elfs_names[id], gifts[g]);
+  }
+  return 0;
 }
 
 /**************************************************************************************************/
 // life of a reindeer
-int reindeer_main(int id, struct SharedStorage* s)
-{
-    printf("Reindeer %s: starting!\n", reindeers_names[id]);
-    while (true) {
-        // TODO ...
-        printf("Left %d\n", s->n_gifts_to_deliver);
+int reindeer_main(int id, struct SharedStorage *s) {
+  printf("Reindeer %s: starting!\n", reindeers_names[id]);
+  while (true) {
+    // TODO ...
 
-        if (s->n_gifts_to_deliver <= 0) {
-            // TODO ...
-            break;
-        } else {
-            s->n_gifts_to_deliver--;
+    sem_wait(&s->mutex);
+    sem_post(&s->mutex);
 
-            int g = 0; // TODO Get gift from queue.
+    if (s->n_gifts_to_deliver <= 0) {
+      // TODO ...
+      break;
+    } else {
+      s->n_gifts_to_deliver--;
 
-            printf("Reindeer %s: I got %s\n", reindeers_names[id], gifts[g]);
-            deliver(g);
-            printf("Reindeer %s: I delivered %s\n", reindeers_names[id], gifts[g]);
-        }
+      int g = 0; // TODO Get gift from queue.
+      sem_wait(&s->sem_filled_slots);
+      sem_wait(&s->mutex);
+
+      g = s->buff[s->begin];
+      s->begin = (s->begin + 1) % BUFFSIZE;
+
+      sem_post(&s->mutex);
+      sem_post(&s->sem_free_slots);
+
+      printf("Reindeer %s: I got %s\n", reindeers_names[id], gifts[g]);
+      deliver(g);
+      printf("Reindeer %s: I delivered %s\n", reindeers_names[id], gifts[g]);
     }
-    return 0;
+  }
+  return 0;
 }
 
 /* ---- Test program ---- */
-int main(void)
-{
-    srand(time(NULL));
+int main(void) {
+  srand(time(NULL));
 
-    // Create shared storage.
-    struct SharedStorage* shared_storage = mmap(
-        NULL,
-        sizeof(struct SharedStorage),
-        PROT_READ | PROT_WRITE,
-        MAP_SHARED | MAP_ANONYMOUS,
-        -1,
-        0);
-    if (shared_storage == MAP_FAILED)
-        syserr("mmap");
+  // Create shared storage.
+  struct SharedStorage *shared_storage =
+      mmap(NULL, sizeof(struct SharedStorage), PROT_READ | PROT_WRITE,
+           MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+  if (shared_storage == MAP_FAILED)
+    syserr("mmap");
 
-    // TODO initialize shared_storage
+  // TODO initialize shared_storage
+  struct SharedStorage *s = shared_storage;
+  s->begin = 0;
+  s->end = 0;
+  sem_init(&s->mutex, true, 1);
+  sem_init(&s->sem_free_slots, true, BUFFSIZE);
+  sem_init(&s->sem_filled_slots, true, 0);
+  s->n_gifts_to_deliver = LOOPS * N_ELFS;
 
-    printf("Creating workers.\nElfs: %d, reindeers: %d\n", N_ELFS, N_REINDEERS);
+  printf("Creating workers.\nElfs: %d, reindeers: %d\n", N_ELFS, N_REINDEERS);
 
-    for (int i = 0; i < N_ELFS + N_REINDEERS; i++) {
-        rand(); // some randomness
-        pid_t pid = fork();
-        ASSERT_SYS_OK(pid);
-        if (!pid) {
-            srand(rand());
-            if (i < N_ELFS)
-                return elf_main(i, shared_storage);
-            else
-                return reindeer_main(i - N_ELFS, shared_storage);
-        }
-        usleep(1 * NAP_MICROSECS);
-        printf("Next worker!\n");
+  for (int i = 0; i < N_ELFS + N_REINDEERS; i++) {
+    rand(); // some randomness
+    pid_t pid = fork();
+    ASSERT_SYS_OK(pid);
+    if (!pid) {
+      srand(rand());
+      if (i < N_ELFS)
+        return elf_main(i, shared_storage);
+      else
+        return reindeer_main(i - N_ELFS, shared_storage);
     }
+    usleep(1 * NAP_MICROSECS);
+    printf("Next worker!\n");
+  }
 
-    for (int i = 0; i < N_ELFS + N_REINDEERS; ++i)
-        ASSERT_SYS_OK(wait(NULL));
+  for (int i = 0; i < N_ELFS + N_REINDEERS; ++i)
+    ASSERT_SYS_OK(wait(NULL));
 
-    assert(shared_storage->n_gifts_to_deliver == 0);
+  assert(shared_storage->n_gifts_to_deliver == 0);
 
-    // TODO clean up shared_storage
+  // TODO clean up shared_storage
 
-    return 0;
+  return 0;
 }
